2bc610638f7f626b426c19cfe381852d15f431dc
Sakomm <sakommuri@gmail.com>
2022-02-01 14:13:21 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,14 @@
+// website to go to https://checkboxolympics.com/ 
+
+//get all buttons
+button = document.querySelectorAll('input[type=checkbox]')
+
+//first two are fake indicating start and stop but doesnt work because reason 
+// cant figure it out 
+
+for( let i = 2; i < button.length; i++){
+  button[i].checked = true;
+  
+}
+
+
